0c897d8b9f49dde3fa781022aa61e86eb3f63ba0
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-08-27 00:19:32 +0700

    node-js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Node JS with Trung Nguyen')
+})
+
+app.get('/about', (req, res) => {
+  res.send('Orgin dev')
 })
 
 app.listen(port, () => {
