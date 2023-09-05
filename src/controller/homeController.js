bb480dc315df266df744792ca837aedf18045501
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-09-05 14:51:56 +0700

    node-js-day8

---
+++ src/controller/homeController.js
@@ -0,0 +1,8 @@
+let getHomepage = (req, res) => {
+    // logic
+    return res.render('index.ejs')
+}
+
+module.exports = {
+    getHomepage
+}
--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRoute from './route/web';
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
+// setup view engine
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
+// init web route
+initWebRoute(app);
 
-app.get('/about', (req, res) => {
-  res.send('Orgin dev')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
